579c9765b82c38898bd23af5e01df0a18d2ef553
Swaroop Hebbar K <hebbarkswaroop@gmail.com>
2026-03-28 10:03:22 +0000

    remove green patches from teh background

--- src/components/background.tsx
+++ src/components/background.tsx
@@ -1,12 +1,10 @@
 "use client"
 
 import { useScroll, useTransform, motion } from "framer-motion"
-import { useEffect, useState } from "react"
 
 export function Background() {
   const { scrollY } = useScroll()
   const y1 = useTransform(scrollY, [0, 5000], [0, 500])
-  const y2 = useTransform(scrollY, [0, 5000], [0, -300])
 
   return (
     <div className="fixed inset-0 z-0 overflow-hidden pointer-events-none">
@@ -20,16 +18,6 @@ export function Background() {
       <motion.div 
         style={{ y: y1 }}
         className="absolute inset-0 grid-bg opacity-20 z-[2]" 
-      />
-
-      {/* Radial Glows */}
-      <motion.div 
-        style={{ y: y2 }}
-        className="absolute top-[-10%] left-[-10%] w-[60%] h-[60%] bg-primary/20 blur-[120px] rounded-full z-[3]" 
-      />
-      <motion.div 
-        style={{ y: y1 }}
-        className="absolute bottom-[-10%] right-[-10%] w-[50%] h-[50%] bg-accent/10 blur-[100px] rounded-full z-[3]" 
       />
     </div>
   )
